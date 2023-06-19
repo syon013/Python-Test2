f830a08337e7780d8a660722a2143e304adb872a
박요진 <syon013@naver.com>
2023-06-19 19:47:12 +0900

    함수 연습

---
+++ byme.py
@@ -0,0 +1,5 @@
+def price(sign):
+    print("이 프로그램은 나로 인해 만들어졌습니다.")
+    print("유튜브 : 없음")
+    print("이메일 : syon013@naver.com")
+    print("{0} + {1} = {2}".format(sign, 2, sign + 2))
---
+++ moduleQuiz.py
@@ -0,0 +1,3 @@
+import byme
+    
+byme.price(4)

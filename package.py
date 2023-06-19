75a9e50d9166b99875b8d7313883e5a3eab79a49
박요진 <syon013@naver.com>
2023-06-19 19:24:46 +0900

    패키지 연습

---
+++ package.py
@@ -0,0 +1,27 @@
+
+
+# import travel.thailand # travel.thailand 패키지의 thailand 모듈을 가져옴
+#import travel.thailand.Thailandpackage # class는 import 불가
+
+#trip_to = travel.thailand.Thailandpackage()
+#trip_to.detail()
+
+# from travel.thailand import Thailandpackage
+# trip_to = Thailandpackage()
+# trip_to.detail()
+
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
+
+# from travel import *
+# #trip_to = vietnam.VietnamPackage()
+# trip_to = thailand.Thailandpackage()
+# trip_to.detail()
+
+# import inspect
+# import random
+
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
